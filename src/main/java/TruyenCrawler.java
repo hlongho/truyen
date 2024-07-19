3f3e8471eabcd6cbfc1e15d4ae1ee87c23e411f3
hlongho <longho4697@gmail.com>
2024-07-19 09:43:28 +0700

    update

--- src/main/java/TruyenCrawler.java
+++ src/main/java/TruyenCrawler.java
@@ -210,7 +210,7 @@ public class TruyenCrawler {
         List<Map<String, String>> chapterContents = new ArrayList<>();
         String nextPageUrl = storyUrl;
         int chapterCount = 0;
-        int chapterCountLimit = 400; // giới hạn số chương tải cho mỗi truyện
+        int chapterCountLimit = 100; // giới hạn số chương tải cho mỗi truyện
 
         // Đọc danh sách các chương hiện có từ chapter.json
         List<Map<String, String>> existingChapters = new ArrayList<>();
